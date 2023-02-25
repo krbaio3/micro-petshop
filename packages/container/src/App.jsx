3954d1060fb8ebcb54cceaa60037ef3be4452401
KrBaIO3 <krbaio3@gmail.com>
2023-02-25 13:54:17 +0100

    Feature/css (#1)
    * feat: css
    
    * feat: generate prefix css class

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -1,8 +1,19 @@
 import React, { Fragment } from 'react';
 import MarketingApp from './components/MarketingApp.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'co',
+});
 export default () => (
-	<Fragment>
-		<MarketingApp />
-	</Fragment>
+	<StylesProvider injectFirst generateClassName={generateClassName}>
+		<BrowserRouter>
+			<Fragment>
+				<Header />
+				<MarketingApp />
+			</Fragment>
+		</BrowserRouter>
+	</StylesProvider>
 );
