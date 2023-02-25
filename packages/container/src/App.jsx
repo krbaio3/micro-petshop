8314604b1b47042787f86d94d216488b93a6229e
J Krbaio <krbaio3@gmail.com>
2023-02-25 13:51:30 +0100

    feat: generate prefix css class

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -2,12 +2,18 @@ import React, { Fragment } from 'react';
 import MarketingApp from './components/MarketingApp.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header.jsx';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'co',
+});
 export default () => (
-	<BrowserRouter>
-		<Fragment>
-			<Header />
-			<MarketingApp />
-		</Fragment>
-	</BrowserRouter>
+	<StylesProvider injectFirst generateClassName={generateClassName}>
+		<BrowserRouter>
+			<Fragment>
+				<Header />
+				<MarketingApp />
+			</Fragment>
+		</BrowserRouter>
+	</StylesProvider>
 );
--- packages/marketing/src/App.jsx
+++ packages/marketing/src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 
 import Landing from './components/Landing.jsx';
 import Pricing from './components/Pricing.jsx';
 
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'ma',
+});
 export default () => {
 	return (
-		<StylesProvider injectFirst>
-			<Router>
+		<StylesProvider injectFirst generateClassName={generateClassName}>
+			<BrowserRouter>
 				<Switch>
 					<Route exact path='/pricing' component={Pricing} />
 					<Route path='/' component={Landing} />
 				</Switch>
-			</Router>
+			</BrowserRouter>
 		</StylesProvider>
 	);
 };
